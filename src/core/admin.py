050b6f6f959734b52d9031c4828b088370ebe70e
Magali Sol Zapata <maguizap2307@gmail.com>
2026-02-05 14:22:06 -0300

    feat: Enhance Curso model with additional fields and create custom UserAdmin in admin

--- src/core/admin.py
+++ src/core/admin.py
@@ -1,10 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Estudiante, Profesor, Curso, Entregable
 
 # Register your models here.
 
 admin.site.register(Estudiante)
 admin.site.register(Profesor)
-admin.site.register(Curso)
 admin.site.register(Entregable)
+#admin.site.register(Curso)
 
+@admin.register(Curso)
+class CursoAdmin(admin.ModelAdmin): 
+    list_display = ('nombre', 'camada', 'fecha_inicio', 'duracion_meses', 'estado') 
+    search_fields = ('nombre', 'camada') 
+    ordering = ['fecha_inicio']
+    list_filter = ('estado',)
+
+
+# Desregistrar el User por defecto y registrar nuestro UserAdmin personalizado
+admin.site.unregister(User)
+@admin.register(User)
+class UserAdmin(BaseUserAdmin): 
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff') 
+    search_fields = ('username', 'first_name', 'last_name', 'email') 
+    readonly_fields = ('date_joined', 'last_login') 
+    fieldsets = ( 
+        (None, {'fields': ('username', 'password')}), 
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}), 
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}), 
+        ('Important dates', {'fields': ('last_login', 'date_joined')}), 
+    )
--- src/core/models.py
+++ src/core/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 class Estudiante(models.Model):
@@ -21,8 +23,20 @@ class Profesor(models.Model):
     
 
 class Curso(models.Model):
+    class Estado(models.TextChoices):
+        ACTIVO = 'AC', 'Activo'
+        INACTIVO = 'IN', 'Inactivo'
+        FINALIZADO = 'FI', 'Finalizado'
     nombre = models.CharField(max_length=100)
     camada = models.IntegerField()
+    fecha_inicio = models.DateField(default=timezone.now)
+    duracion_meses = models.IntegerField(default=1)
+    estado = models.CharField(
+        max_length=2,
+        choices=Estado.choices,
+        default=Estado.INACTIVO,
+    )
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.nombre
